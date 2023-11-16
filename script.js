df0a0797b961da37b4a21f204f578c2343f70e08
Cr1saf-edu <csacosta@uniguajira.edu.co>
2023-11-15 22:13:43 -0500

    Add animations
    Add animations

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
-$('.js-next').on('click', function() {
-	var current = $('.page.active');
-	var findNext = $(current).next(".page");
-	$(current).removeClass('active');
-	setTimeout(function() {
-		$(findNext).addClass("active");
-	}, 200);
-});
+var autoTransitionInterval;
 
-$('.js-prev').on('click', function() {
-	var current = $('.page.active');
+function nextPage() {
+	var current = $(".page.active");
+	var findNext = $(current).next(".page");
+
+	$(current).removeClass("active");
+
+	setTimeout(function () {
+		if ($(findNext).length) {
+			$(findNext).addClass("active");
+		} else {
+			$(".page:first").addClass("active");
+		}
+	}, 200);
+}
+
+function prevPage() {
+	var current = $(".page.active");
 	var findPrev = $(current).prev(".page");
-	$(current).removeClass('active');
-	setTimeout(function() {
+
+	$(current).removeClass("active");
+
+	setTimeout(function () {
 		$(findPrev).addClass("active");
 	}, 200);
+}
+
+$('.js-next').on('click', function () {
+	nextPage();
 });
 
-$('.js-back-to-1').on('click', function() {
+$('.js-prev').on('click', function () {
+	prevPage();
+});
+
+$('.js-back-to-1').on('click', function () {
 	var current = $('.page.active');
 	$(current).removeClass('active');
-	setTimeout(function() {
+	setTimeout(function () {
 		$('.page-cover').addClass("active");
 	}, 400);
 });
